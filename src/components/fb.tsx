5a46e6f8ae70aff0d3978a645bde3f59fe02293f
fialhoFabio <fabiom456@gmail.com>
2025-12-15 15:29:01 -0300

    fix: update loginOnFacebook function to include config_id in logging and request parameters

--- src/components/fb.tsx
+++ src/components/fb.tsx
@@ -52,10 +52,10 @@ export const FacebookLoader = () => {
   }
   function loginOnFacebook() {
     console.log({
+        config_id: import.meta.env.WAKU_PUBLIC_FB_CONFIG_ID || '',
         response_type: 'code',
         override_default_response_type: true,
         extras: {
-          config_id: import.meta.env.WAKU_PUBLIC_FB_CONFIG_ID || '',
           setup: {},
         },
         // scope: 'whatsapp_business_messaging,whatsapp_business_management',
@@ -65,10 +65,10 @@ export const FacebookLoader = () => {
         console.log('FB login: ', res);
         set_wb_code(res.authResponse?.code);
       }, {
+        config_id: import.meta.env.WAKU_PUBLIC_FB_CONFIG_ID || '',
         response_type: 'code',
         override_default_response_type: true,
         extras: {
-          config_id: null,
           setup: {},
         },
         // scope: 'whatsapp_business_messaging,whatsapp_business_management',
