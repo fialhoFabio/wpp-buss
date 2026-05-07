9a3f61a70559f0adc0022013deb1cd0ba6e24640
fialhoFabio <fabiom456@gmail.com>
2026-05-07 15:21:32 -0300

    feat: remove fixed positioning from footer for improved layout flexibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Link } from 'waku';
 
 export const Footer = () => {
   return (
-    <footer className='border-t border-gray-100 bg-white px-6 py-4 lg:fixed lg:bottom-0 lg:left-0 lg:right-0'>
+    <footer className='border-t border-gray-100 bg-white px-6 py-4'>
       <div className='mx-auto flex max-w-7xl items-center justify-between text-xs text-gray-400'>
         <span>© {new Date().getFullYear()} Mdmed Alerts</span>
         <Link to='/privacy-policy' className='hover:text-gray-600'>
